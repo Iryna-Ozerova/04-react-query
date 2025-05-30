58c556e54853d1fa4e150f132884302ccf17629d
Irina Ozerova <marta.ilyashenko@gmail.com>
2025-05-31 00:34:53 +0300

    new commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./сomponents/App/App"; 
+import App from "./components/App/App"; 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
